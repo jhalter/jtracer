620ae48f7e6cf7e6d986c95ed8113cc2c12ef8a9
Jeff Halter <868228+jhalter@users.noreply.github.com>
2022-11-23 21:42:42 -0800

    Drop unused PPM output format in favor of image std lib

--- canvas.go
+++ canvas.go
@@ -1,12 +1,10 @@
 package jtracer
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"os"
-	"strings"
 )
 
 type Canvas struct {
@@ -29,37 +27,6 @@ func (c *Canvas) WritePixel(x, y int, color *Color) {
 
 func (c *Canvas) PixelAt(x int, y int) *Color {
 	return &c.Data[y][x]
-}
-
-const ppmHeader = `
-P3
-%d %d
-255
-`
-
-func (c *Canvas) ToPPM() string {
-	var ppm strings.Builder
-	ppm.WriteString(fmt.Sprintf(ppmHeader, c.Width, c.Height))
-
-	for y := range c.Data {
-		var line strings.Builder
-		leadingSpace := ""
-		for x := range c.Data[y] {
-			color := c.Data[y][x]
-			red, green, blue := color.Normalize()
-			line.WriteString(fmt.Sprintf("%s%d %d %d", leadingSpace, red, green, blue))
-			leadingSpace = " "
-			if line.Len() > 70 {
-				leadingSpace = "\n"
-				ppm.WriteString(line.String())
-				line.Reset()
-			}
-		}
-		ppm.WriteString(line.String())
-		ppm.WriteString("\n")
-	}
-	ppm.WriteString("\n")
-	return ppm.String()
 }
 
 func (c *Canvas) ToImage() image.Image {
